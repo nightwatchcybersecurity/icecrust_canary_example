5d9ccc13f071d0978fcbe6a4acdf825c9df46025
nightwatchcyber <research@nightwatchcybersecurity.com>
2021-05-13 20:46:41 -0400

    another shell script

--- scripts/verify_all.sh
+++ scripts/verify_all.sh
@@ -9,7 +9,7 @@ cd $SCRIPT_DIR/../docs
 for input_file in data/input/*.json
 do
 	filename=`echo $input_file | sed -e 's/data\/input\///g' | sed -e 's/\.json//g'`
-	echo ---- Processing $input_file.json ----
+	echo ---- Processing "$input_file.json" ----
 	icetrust canary $1 "data/input/$filename.json" --output-json "data/output/$filename.json" --save-file "data/output_data/$filename.txt" || true
 	echo
 	#break
